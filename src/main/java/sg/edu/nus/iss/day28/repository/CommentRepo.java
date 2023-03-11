5d2cd8b2f1ea777e41dbb446487c56b020f63eb0
haha27lim <eileen2708@hotmail.com>
2023-03-11 17:28:28 +0800

    updated

--- src/main/java/sg/edu/nus/iss/day28/repository/CommentRepo.java
+++ src/main/java/sg/edu/nus/iss/day28/repository/CommentRepo.java
@@ -29,23 +29,26 @@ public class CommentRepo {
 						$meta: "textScore"
 					}
 				}
-			)
+			).limit(3)
 	 */
     public List<Document> searchCommentText(String... texts) {
 
+		// Print out each text string being searched for
 		for (int i = 0; i < texts.length; i++) 
 			System.out.printf(">>> texts[%d]: %s\n", i, texts[i]);
 
+		// Create a TextCriteria object for the default language that matches any of the text strings
 		TextCriteria criteria = TextCriteria.forDefaultLanguage()
 				.matchingAny(texts);
 
+		// Create a TextQuery object with the above criteria, including the score field and sorting by score
 		TextQuery textQuery = TextQuery.queryText(criteria)
 				// Use this for method chaining
 				.includeScore(FIELD_TEXT_SCORE)
 				.sortByScore();
 
 		//textQuery.setScoreFieldName(FIELD_TEXT_SCORE);
-
+		// Set a limit of 3 for the number of comments to be returned
 		Query query = textQuery.limit(3);
 
 		return template.find(query, Document.class, COLLECTION_COMMENTS);
